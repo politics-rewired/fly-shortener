78569d230c45e931f8b0a810bd45ac9da32c9c32
Benjamin Chrobot <benjamin.chrobot@alum.mit.edu>
2021-03-07 17:50:12 -0500

    chore: add eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2021,
+  },
+  extends: ['plugin:@typescript-eslint/recommended', 'prettier', 'plugin:prettier/recommended'],
+};
